cf76775c81110becd78114218af482743a47cdec
Shivani Bodara <shivanibodara1@gmail.com>
2025-04-01 14:07:07 -0400

    Implemented get method

--- router/friends.js
+++ router/friends.js
@@ -11,10 +11,7 @@ let friends = {
 
 // GET request: Retrieve all friends
 router.get("/",(req,res)=>{
-
-  // Update the code here
-
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    res.send(JSON.stringify(friends, null, 4));
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
